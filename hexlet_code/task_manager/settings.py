2998b4842392d8552483ee54ad50d41b4794b3e9
negodinaarina <negodinaarina24@gmail.com>
2023-08-23 17:57:56 +0300

    config for railway deployment

--- hexlet_code/task_manager/settings.py
+++ hexlet_code/task_manager/settings.py
@@ -9,15 +9,13 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+from dotenv import load_dotenv
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+load_dotenv(os.path.join(BASE_DIR, ".env"))
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-+no=dth&_hiyi5kwux#+j6mk=83g!m50d=%!$-hds8k^njj(l3'
